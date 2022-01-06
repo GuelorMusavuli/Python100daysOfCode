526608ca56238af305b870df104eed278076bb55
GuelorMusavuli <guelormusavuli2@gmail.com>
2022-01-06 14:31:25 +0300

    Final touches

--- day_10/calculator_challenge.py
+++ day_10/calculator_challenge.py
@@ -1,3 +1,6 @@
+from art import logo
+
+
 # Add
 def add(n1, n2):
     return n1 + n2
@@ -26,22 +29,29 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
 
-# Print the first result
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+def calculator():
+    print(logo)
 
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What is the nest number?:"))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(first_answer, num3)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-# Print the second result
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+    while should_continue:
+        operation_symbol = input("Pick an operation : ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
+        # Print the first result
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer} or type 'n' to start afresh.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()  # Recursion function to start all the way back from the beginning
+
+
+calculator()
