12c3d09ad07397e2e81731e68f24f36e80d92a1e
GuelorMusavuli <guelormusavuli2@gmail.com>
2022-01-06 13:30:27 +0300

    Extend the calculation of the calculator challenge

--- day_10/calculator_challenge.py
+++ day_10/calculator_challenge.py
@@ -32,7 +32,16 @@ for symbol in operations:
 operation_symbol = input("Pick an operation from the line above: ")
 num2 = int(input("What's the second number?: "))
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+first_answer = calculation_function(num1, num2)
 
-# Print the result
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+# Print the first result
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What is the nest number?:"))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(first_answer, num3)
+
+# Print the second result
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+
